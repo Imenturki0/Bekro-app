6403aaf7f014706753649935cb2fdfd57d85a07f
samar <samaromm98@gmail.com>
2023-03-10 00:42:19 +0300

    login done

---
+++ lib/screens/check_auth_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../screens/main_screen.dart';
+import '../screens/user_profile.dart';
+
+class CheckAuth extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+
+    User? firebaseUser = FirebaseAuth.instance.currentUser;
+    Widget firstWidget;
+    if (firebaseUser != null) {firstWidget = UserProfile();}
+    else {firstWidget = MainScreen();}
+
+    return Scaffold(
+        body:firstWidget
+    );
+  }
+}
+
